c49430e6b809eca4095c500101411c4cf91322fe
salijgerenaia <113972180+salijgerenaia@users.noreply.github.com>
2024-06-30 15:41:54 +0400

    added

--- src/app/exp-forms/exp-forms.component.ts
+++ src/app/exp-forms/exp-forms.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from '@angular/material/button';
@@ -31,60 +31,57 @@ import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/c
 export class ExpFormsComponent {
 
   arr: Company[] = [];
+  jobForm: FormGroup;
+  positionForm: FormGroup;
+  addJob = false;
+  addPosition = false;
 
-  addPost () {
-    this.arr.push({
-      name: this.applicationInfo.companyName,
-      url: this.applicationInfo.companyUrl,
-      desc: this.applicationInfo.companyDesc,
-      posName: this.applicationInfo.posName,
-      posLevel: this.applicationInfo.posLevel,
-      posDesc: this.applicationInfo.posDesc,
-      dateFrom : this.applicationInfo.fromDate,
-      dateTo: this.applicationInfo.toDate
+  constructor(private fb: FormBuilder) {
+    this.jobForm = this.fb.group({
+      companyName: ['', Validators.required],
+      companyWeb: ['', Validators.required],
+      companyDesc: ['']
+    });
 
-    })
-
+    this.positionForm = this.fb.group({
+      posName: ['', Validators.required],
+      posLevel: ['', Validators.required],
+      posDesc: [''],
+      fromDate: ['', Validators.required],
+      toDate: ['', Validators.required]
+    });
   }
 
-applicationInfo = {
-    companyName : '',
-  companyUrl : '',
-  companyDesc : '',
-  posName : '',
-  posLevel : '',
-  posDesc : '',
-  fromDate: '',
-  toDate: ''
+  add() {
+    this.addJob = true;
+  }
 
+  addPos() {
+    this.addPosition = true;
+  }
 
+  addPost() {
+    if (this.positionForm.valid) {
+      this.arr.push({
+        name: this.jobForm.value.companyName,
+        url: this.jobForm.value.companyWeb,
+        desc: this.jobForm.value.companyDesc,
+        posName: this.positionForm.value.posName,
+        posLevel: this.positionForm.value.posLevel,
+        posDesc: this.positionForm.value.posDesc,
+        dateFrom: this.positionForm.value.fromDate,
+        dateTo: this.positionForm.value.toDate
+      });
+
+      // Reset forms and state after adding
+      this.jobForm.reset();
+      this.positionForm.reset();
+      this.addJob = false;
+      this.addPosition = false;
+    }
+  }
+
+  deleteJob(index: number) {
+    this.arr.splice(index, 1);
+  }
 }
-
-
-
-
-  addJob : boolean = false
-  addPosition : boolean = false
-
-
-
-  add () {
-    this.addJob = true
-
-  }
-
-  addPos () {
-    this.addPosition = true
-
-  }
-
-  constructor(private fb: FormBuilder) {}
-
-
-  jobForm = this.fb.group({
-    companyName : '',
-    companyWeb : '',
-    companyDesc : ''
-  })
-}
-
