27acc7f590fd2d313b71ea5f85d50b317733ee89
Giorgi81 <147637853+Giorgi81@users.noreply.github.com>
2024-07-01 12:02:19 +0400

    1

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,27 +1,45 @@
 import {Component, OnInit} from '@angular/core';
-import {MatFormField} from "@angular/material/form-field";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
   standalone: true,
   imports: [
-    MatFormField
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatFormFieldModule,
+    MatButton,
+    ReactiveFormsModule
   ],
   styleUrls: ['./match.component.scss']
 })
-export class MatchComponent implements OnInit{
-  ngOnInit() {
+export class MatchComponent implements OnInit {
+  wordsFormControl: FormControl = new FormControl('');
+  wordFormControl: FormControl = new FormControl('');
+  words: string[] = [];
+  percents: string[] = [];
 
-    const name = 'ხე';
-    const names = ['ხე', 'ხეს', 'ცდ'];
-    let matchedIndexes = [];
+  ngOnInit() {
+    this.wordsFormControl.valueChanges.subscribe(_ => {
+      this.words = _.split(',');
+      this.words = this.words.map(_ => _.trim());
+    })
+  }
+
+  public matchWithPercent(): void {
+    const name: string = this.wordFormControl.value;
+    const names: string[] = this.words;
+    let matchedIndexes: any[] = [];
     let index;
-    let indexesByNameLength : any[] = []
+    let indexesByNameLength: any[] = []
 
     for (let i = 0; i < names.length; i++) {
       matchedIndexes.push(indexesByNameLength);
-
 
       for (let j = 0; j < names[i].length; j++) {
 
@@ -34,12 +52,11 @@ export class MatchComponent implements OnInit{
 
     let percents = [];
     for (let i of matchedIndexes) {
-      const matched = i.filter(a => a !== -1).length;
+      const matched = i.filter((a: any) => a !== -1).length;
       const percent = (matched / i.length) * 100 + '%'
       percents.push(percent)
     }
-
-    console.log(percents);
+    this.percents = percents
   }
 
 }
